6c94173657f4780d1891efbfa8417724e23a9c07
DanyaVernadsky <danya-sten151@ya.ru>
2018-10-01 12:23:52 +0300

    Added enter array, testing, fixed algoritm of searching - now it works right.

--- Homework-3.3/Homework-3.3/Homework-3.3.cpp
+++ Homework-3.3/Homework-3.3/Homework-3.3.cpp
@@ -5,30 +5,57 @@ int searchMaxÑomb(int array[], int length)
 {
 	int maxComb = 0;
 	int result = 0;
-	int* array2 = new int[length] {};
 	for (int i = 0; i < length; ++i)
 	{
-		for (int j = 0; j < length - i; ++j)
-		{
-			if ((array[j] == array[i])&&(array[j] != _CRT_INT_MAX))
+		int count = 0;
+		for (int j = i + 1; j < length; ++j)
+		{//Ïîìå÷àş ìàêñèìóìîì ıë-òû êîòîğûå óæå ïîñ÷èòàë
+			if ((array[i] == array[j])&&(array[j] != _CRT_INT_MAX))
 			{
-				int valueSecondArray = ++array2[i];
-				if (maxComb < valueSecondArray)
+				count++;
+				if (maxComb < count)
 				{
-					maxComb = valueSecondArray;
+					maxComb = count;
 					result = i;
+					array[j] = _CRT_INT_MAX;
 				}
 			}
 		}
-		array[i] = _CRT_INT_MAX;
 	}
 	return array[result];
 }
 
+bool testing()
+{
+	const int n = 10;
+	int array[n]{ 1, 5, 3, 4, 4, 1, 0, 4, 5, 7 };
+	int result = 4;
+	if (result != searchMaxÑomb(array, n))
+	{
+		return false;
+	}
+	return true;
+}
 
 int main() {
-	const int n = 10;
-	int array[n]{ 3, 2, 5, 3, 1, 0, -1, 2, 5, 5 };
+	if (testing())
+	{
+		printf("%s\n", "Testing is positive");
+	}
+	else
+	{
+		printf("%s\n", "Testing is negative");
+	}
+	printf("%s", "Enter length of array: ");
+	int n = 0;
+	scanf("%i", &n);
+	int* array = new int[n];
+	printf("%s", "Enter array: ");
+	for (int i = 0; i < n; ++i)
+	{
+		scanf("%i", &array[i]);
+	}
+	printf("%s", "Result: ");
 	printf("%i", searchMaxÑomb(array, 10));
 	scanf("%i", &n);
 	return 0;
