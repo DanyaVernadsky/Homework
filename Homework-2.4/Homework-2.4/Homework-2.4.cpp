7d923d0556158544bfd168b5042b23d51ef918b3
DanyaVernadsky <danya-sten151@ya.ru>
2018-10-01 22:07:49 +0300

    Just little changes for "printf" function

--- Homework-2.4/Homework-2.4/Homework-2.4.cpp
+++ Homework-2.4/Homework-2.4/Homework-2.4.cpp
@@ -58,7 +58,7 @@ bool testing()
 	int array1[1]{ 5 };
 	int result1[1]{ 5 };
 	algoritm(array1, 1);
-	return (array[0] == result[0])
+	return (array[0] == result[0]);
 }
 
 int main()
@@ -67,22 +67,22 @@ int main()
 	int array[n]{4, 5, 3, 2, 7, 8, 2, 1, 0, 4};
 	if (testing())
 	{
-		printf("%s\n", "Testing is positive! :)");
+		printf("Testing is positive! :)\n");
 	}
 	else
 	{
-		printf("%s\n", "Testing is negative! :(");
+		printf("Testing is negative! :(\n");
 	}
 	randomArray(array, n);
 
-	printf("%s\n", "Output random array");
+	printf("Output random array\n");
 	for (int i = 0; i < n; ++i)
 	{
 		printf("%i ", array[i]);
 	}
 	printf("\n");
 
-	printf("%s\n", "Output sort array");
+	printf("Output sort array\n");
 	algoritm(array, n);
 	for (int i = 0; i < n; ++i)
 	{
